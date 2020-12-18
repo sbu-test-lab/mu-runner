2186e01975f1655c99350db5cb0063f761d3b9b8
mgh <m.ghoreshi@gmail.com>
2020-12-18 19:37:19 +0330

    first version project now works

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # java-mutant-runner
 Seed a bug in one of your java class files (called a mutant). Then, make a list of these buggy java classes (mutants). Now you can use `java-mutant-runner` for compiling and running tests:
-* `java-mutant-runner` replaces each buggy file (mutant) with orignal java source file.
+* `java-mutant-runner` replaces each buggy file (mutant) with original java source file.
 * After replacing a buggy file, `java-mutant-runner` compiles our project.
 * Now `java-mutant-runner` runs your JUnit test suites.
-* At the end, `java-mutant-runner` reports you, which bugg class is dicoverd by your tests (killed by your tests)
+* At the end, `java-mutant-runner` reports you, which buggy class is discovered by your tests (killed by your tests)
--- src/main/java/Mutant.java
+++ src/main/java/Mutant.java
@@ -8,6 +8,12 @@ public class Mutant {
     File file;
     String type;
     String number;
+    String pathAndFileName;
+    private String filename;
+    //killed, not-verify, live, compile-error
+    private String status="not-verify";
+    private int numberOfTestRun;
+    private int numberOfTestFails;
 
     public Mutant(File file) {
         this.file = file;
@@ -20,6 +26,12 @@ public class Mutant {
         this.number=nameWithoutExtension.substring(nameWithoutExtension.lastIndexOf('_')+1);
         String nameWithoutNumber=nameWithoutExtension.substring(0,name.lastIndexOf('_'));
         this.type=nameWithoutNumber.substring(nameWithoutNumber.lastIndexOf('_')+1);
+        String onlyName=nameWithoutNumber.substring(0,nameWithoutNumber.lastIndexOf('_'));
+        this.pathAndFileName=onlyName.replaceAll("\\.","/")+".java";
+        if(this.pathAndFileName.contains("/"))
+            this.filename=this.pathAndFileName.substring(this.pathAndFileName.lastIndexOf('/')+1);
+        else
+            this.filename=this.pathAndFileName;
     }
 
     public String getFileName(){
@@ -27,7 +39,7 @@ public class Mutant {
     }
 
     public String getSimpleName(){
-        return this.type+'-'+this.number;
+        return this.filename+ " - "+this.type+" - "+this.number;
     }
 
     public File getFile() {
@@ -42,4 +54,54 @@ public class Mutant {
         return number;
     }
 
+    public String getPathAndFileName() {
+        return pathAndFileName;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    @Override
+    public String toString() {
+        return "Mutant{" +
+                "file=" + file +
+                ", type='" + type + '\'' +
+                ", number='" + number + '\'' +
+                ", pathAndFileName='" + pathAndFileName + '\'' +
+                ", filename='" + filename + '\'' +
+                '}';
+    }
+
+    public void setStatus(String s) {
+        this.status=s;
+    }
+
+    public String getStatus() {
+        return this.status;
+    }
+
+    public void setNumberOfTestRun(int i) {
+        this.numberOfTestRun=i;
+    }
+
+    public void incNumberOfTestRun(int i) {
+        this.numberOfTestRun=this.numberOfTestRun+i;
+    }
+
+    public int getNumberOfTestRun() {
+        return this.numberOfTestRun;
+    }
+
+    public void setNumberOfTestFails(int i) {
+        this.numberOfTestFails=i;
+    }
+
+    public void incNumberOfTestFails(int i) {
+        this.numberOfTestFails=this.numberOfTestRun+i;
+    }
+
+    public int getNumberOfTestFails() {
+        return this.numberOfTestFails;
+    }
 }
